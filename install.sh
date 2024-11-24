d418e0df6c3b7eb329600f3d7870deeb3e2888ba
GeoNew2011 <152255543+GeoNew2011@users.noreply.github.com>
2024-11-24 16:48:56 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -38,7 +38,7 @@ fi
 docker run hello-world
 echo "Docker is working properly"
 echo "Installing manager..."
-wget "https://github.com/GeoNew2011/Docker-container-manager/releases/download/v1.0/scripts.tar.gz" -o scripts.tar.gz
+wget "https://github.com/GeoNew2011/Docker-container-manager/releases/download/v1.0/scripts.tar.gz"
 mkdir /usr/share/dockerManager
 tar -xzf scripts.tar.gz -C /usr/share/dockerManager
 chmod -R u+x /usr/share/dockerManager
