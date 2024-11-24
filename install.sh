d883c407aed6b1e36a75212c412da91d90c85e0e
GeoNew2011 <152255543+GeoNew2011@users.noreply.github.com>
2024-11-24 16:35:52 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,24 +1,30 @@
 #!/usr/bin/bash
 
+#Check root
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
 #Unistall unofficial Docker
-for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do sudo apt-get -yqq remove $pkg; done
+for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do  apt-get -yqq remove $pkg; done
 
 # Add Docker's official GPG key:
-sudo apt-get update
-sudo apt-get install ca-certificates curl
-sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
-sudo chmod a+r /etc/apt/keyrings/docker.asc
+ apt-get update
+ apt-get install ca-certificates curl
+ install -m 0755 -d /etc/apt/keyrings
+ curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+ chmod a+r /etc/apt/keyrings/docker.asc
 
 # Add the repository to Apt sources:
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
+  tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update
 
 #Install Docker + dependencies
-sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin python curl
+apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin python3 wget
 
 #Checking that Docker is installed successfully
 export docker_output=$(docker run hello-world)
@@ -32,7 +38,7 @@ fi
 docker run hello-world
 echo "Docker is working properly"
 echo "Installing manager..."
-curl "https://github.com/GeoNew2011/Docker-container-manager/releases/download/v1.0/scripts.tar.gz" -o scripts.tar.gz
+wget "https://github.com/GeoNew2011/Docker-container-manager/releases/download/v1.0/scripts.tar.gz" -o scripts.tar.gz
 mkdir /usr/share/dockerManager
 tar -xzf scripts.tar.gz -C /usr/share/dockerManager
 chmod -R u+x /usr/share/dockerManager
